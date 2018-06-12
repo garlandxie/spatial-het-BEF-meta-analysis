15150cbe05c9cc3de59854c9533a108545dfaaac
garlandxie <garland.xie@gmail.com>
2018-06-12 10:42:42 -0300

    Cleaned up accented characters due to BibTEX encoding

--- src/0-cleaning_lit_search.R
+++ src/0-cleaning_lit_search.R
@@ -14,17 +14,76 @@ path <- here("data/original/prisma_protocol", "lit_search_mendeley.bib")
 # Convert bibtex into a tibble 
 bib <- bib2df(path)
 
-# Remove any unncessary columns; keep the metadata to a minimal
+# clean data 
 bib_clean <- bib %>% 
-  select(author = AUTHOR, 
-         journal = JOURNAL, 
-         vol = VOLUME, 
-         num = NUMBER, 
-         title = TITLE, 
-         doi = DOI, 
-         url = URL) 
+  
+  # Remove any unncessary columns; keep the metadata to a minimal
+  select(author = "AUTHOR", 
+         journal = "JOURNAL", 
+         vol = "VOLUME", 
+         num = "NUMBER", 
+         pages = "PAGES",
+         title = "TITLE", 
+         doi = "DOI") %>%
+  
+  # author column is in a list; collapse the author list and turn into character strings 
+  mutate(author = unlist(map(author, function(x) paste(unlist(x), collapse = "; ")))) %>%
+  
+  # bibtex contains some unique encoding for accents; clean it up using regular expressions
+  # need to find a more efficient method later on; revisit gsubfn 
+  
+  # a 
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{a}}"),   replacement = "á")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed('{\\"{a}}'),   replacement = "ä")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed('{\\`{a}}'),   replacement = "à")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed('{\\~{a}}'),   replacement = "ã")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{A}}"),   replacement = "ã")) %>%
+  
+  # c
+  mutate(author = str_replace_all(author, pattern = fixed("{\\c{c}}"),   replacement = "ç")) %>%
+  
+  #e
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{e}}"),   replacement = "é")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{e}"),    replacement = "é")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\^{e}}"),   replacement = "ê")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\^{E}}"),   replacement = "Ê")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed('{\\"{e}}'),   replacement = "ë")) %>%
+  
+  #i
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{i}}"),   replacement = "í")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed('{\\"{i}}'),   replacement = "ï")) %>%
+ 
+  
+  #l
+  mutate(author = str_replace_all(author, pattern = fixed("{\\l}"),     replacement = "ł")) %>%
+  
+  # o
+  mutate(author = str_replace_all(author, pattern = fixed('{\\"{o}}'),   replacement = "ö")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\^{o}}"),   replacement = "ô")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{o}}"),   replacement = "ó")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\o}"),      replacement = "ø")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed('{\"{O}}'),    replacement = "Ö")) %>%
+  
+  
+  # u
+  mutate(author = str_replace_all(author, pattern = fixed('{\\"{u}}'),   replacement = "ü")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{u}}"),   replacement = "ú")) %>%
+  
+  # s
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{s}}"),   replacement = "ś")) %>%
+  mutate(author = str_replace_all(author, pattern = fixed("{\\'{y}}"),   replacement = "ỳ")) 
 
-# Manually clean data 
-bib_clean$doi
+
+
+
+  
+"{\v{s}}"
+"{\v{e}}"
+'{\"{i}}'
+
+
+
+ 
+
 
 
