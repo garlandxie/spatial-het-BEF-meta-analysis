c93602403d7b41b8f57d509f7b387d9db0959319
garlandxie <garland.xie@gmail.com>
2019-10-23 16:01:00 -0400

    Added some scripts

--- src/00-source.R
+++ src/00-source.R
@@ -4,3 +4,11 @@ library(here)
 # import files ----------------------------------------------------------------- 
 source(here("src", "01-01-import-Lundholm-2015.R"))
 source(here("src", "01-02-import-Cadotte-2013.R"))
+source(here("src", "01-03-import-Blesh-2018.R"))
+source(here("src", "01-04-import-Jena-2010.R"))  
+
+# escalc -----------------------------------------------------------------------
+source(here("src", "02-escalc.R"))
+       
+       
+       
