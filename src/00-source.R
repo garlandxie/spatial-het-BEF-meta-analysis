2d22ff270622e4507b26732bf7f6185fed567cf6
garlandxie <garland.xie@gmail.com>
2019-10-23 15:05:33 -0400

    Initial commit

---
+++ src/00-source.R
@@ -0,0 +1,6 @@
+# libraries --------------------------------------------------------------------
+library(here)
+
+# import files ----------------------------------------------------------------- 
+source(here("src", "01-01-import-Lundholm-2015.R"))
+source(here("src", "01-02-import-Cadotte-2013.R"))
