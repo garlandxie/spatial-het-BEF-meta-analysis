349a9c6c2409585a756cf504c30e336ba96e7c2e
garlandxie <garland.xie@gmail.com>
2019-10-29 13:32:42 -0400

    Added diagnostic tests and custom-made forest plot

--- src/03-meta-analysis.R
+++ src/03-meta-analysis.R
@@ -1,12 +1,15 @@
-# libraries ----
+# libraries --------------------------------------------------------------------
 library(metafor)
 library(here)
 library(readr)
+library(dplyr)
+library(ggplot2)
+library(forcats)
 
-# import ----
+# import -----------------------------------------------------------------------
 df <- read_csv(here("data/final", "escalc_df.csv"))
 
-# calculate effect sizes ----
+# calculate effect sizes -------------------------------------------------------
 
 esc_df <- escalc(measure = "CVR",
        
@@ -18,12 +21,100 @@ esc_df <- escalc(measure = "CVR",
        
        data = df)
 
-# meta-regression: inference ----
+esc_df <- esc_df %>%
+        rename(lnCV_ES  = yi, # effect size estimate
+               samp_var = vi) # corresponding sampling variance
+
+# meta-regression: inference ---------------------------------------------------
 
 # without moderator variables 
-rma1 <- rma.uni(yi = yi, vi = vi, data = esc_df)
+# REML for tau-squared estimation: recommended by Viechtbauer (2005)
+# Use weighted least squares: weighted = TRUE
+rma1 <- rma(yi = lnCV_ES,
+            vi = samp_var,
+            method = "REML", 
+            weighted = TRUE, 
+            test = "knha",
+            level = 95,
+            data = esc_df)
+
+# get tau-squared, I-squared, H-squared, overall effect size + CI's
 summary(rma1)
-forest(rma1)
-funnel(rma1)
+
+# 95% Wald confidence intervals 
+confint(rma1)
+
+# funnel plot variations 
+# applies only to rma models without mods 
+funnel(rma1, main = "Standard Error")
+funnel(rma1, yaxis = "vi", main = "Sampling Variance")
+funnel(rma1, yaxis = "seinv", main = "Inverse Standard Error")
+funnel(rma1, yaxis = "vinv", main = "Inverse Sampling Variance")
+
+# plot of the (restricted) log-likelhood as a function of tau-squared
+profile.rma.uni(rma1)
+
+# funnal plot assymmetry
+trimfill.rma.uni(rma1)
+
+# forest plot ------------------------------------------------------------------
+
+# build df for a ggplot2-based forest plot
+forest_df <- rbind(
+        
+        # effect size and SE for each study
+        data.frame(ES    = rma1$yi,
+                   SE    = sqrt(rma1$vi),
+                   Type  = "Study", 
+                   Study = paste(esc_df$source, 
+                                 esc_df$location, 
+                                 sep = " ")
+                   ),
+        
+        # overall effect size estimate across all studies
+        data.frame(ES    = rma1$b,
+                   SE    = rma1$se,
+                   Type  = "Summary",
+                   Study = "Summary")
+        )
+
+# convert to factor for plotting purposes
+forest_df <- forest_df %>%
+        mutate(Study2 = factor(Study, rev(levels(Study))))
+
+# create forest plot
+custom_forest <- forest_df %>%
+        ggplot(aes(
+                x = Study2,
+                y = ES,
+                ymax = ES + (1.96 * SE),
+                ymin = ES - (1.96 * SE),
+                colour = factor(Type)
+        )) +
+        geom_pointrange() +
+        coord_flip() +
+        geom_hline(
+                yintercept = 0,
+                lty = 2,
+                size = 1,
+                colour = "grey"
+        ) +
+        xlab("Study") +
+        ylab("Log CV Ratio") +
+        scale_colour_manual(values = c("grey", "black")) +
+        theme(
+                legend.position = "none",
+                panel.border = element_rect(fill = NA),
+                panel.background = element_blank(),
+        )
+
+# save the plot! ---------------------------------------------------------------
+
+# ggplot2-based forest plot
+ggsave(here("output/figures", "forest-plot.png"),
+       plot = custom_forest,
+       height = 7,
+       width = 5,
+       device = "png")
 
 
